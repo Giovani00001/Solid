a88c79190f301170bf186299dd5f6ca52c5bfad6
Giovani <Giip00001@gmail.com>
2023-10-30 00:19:36 -0600

    principio de abierto y cerrado

--- 2-OpenClose/EmployeeContractor.cs
+++ 2-OpenClose/EmployeeContractor.cs
@@ -1,8 +1,8 @@
 namespace OpenClose
 {
-    public class EmployeeFullTime : Employee
+    public class EmployeeContractor : Employee
     {
-        public EmployeeFullTime(string fullname, int hoursWorked)
+        public EmployeeContractor(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
@@ -10,10 +10,9 @@ namespace OpenClose
 
         public override decimal CalculateSalaryMonthly()
         {
-            decimal hourValue = 30000M;
+            decimal hourValue = 20000M;
             decimal salary = hourValue * HoursWorked;
-            return salary;
-          
+            return salary; 
         }
     }
 }
